28dab82b71ae3c0a0eeb997358fc133c635c3906
xjw1001001 <1176434052@qq.com>
2017-09-25 12:10:33 +0800

    111

--- RunSimulation.py
+++ RunSimulation.py
@@ -32,7 +32,7 @@ def main(args):
     test = ReCodonGeneconv( newicktree, alignment_file, paralog, Model = 'MG94', Force = None, clock = False, save_name = save_name,IGC_geo = IGC_geo, sim_num = sim_num)
     
 
-    #test.get_mle(False, True, 0, 'BFGS')
+    test.get_mle(False, True, 0, 'BFGS')
     test.site_reconstruction()
     test.get_individual_summary(summary_path = './SimulationSummary/' + '_'.join(paralog) + '/', file_name = summary_name)
     
