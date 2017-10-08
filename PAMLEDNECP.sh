421797de56ef3df29a9ba8c2e874d5ad385bfd66
Jingwie Xiong (Jeff Thorne) <jxiong7@captainkirk.statgen.ncsu.edu>
2017-10-08 04:26:23 -0400

    sbatchs

--- PAMLEDNECP.sh
+++ PAMLEDNECP.sh
@@ -1,4 +1,4 @@
-#!/bin/bash[0.0,0.1,0.7, 1.0, 0.4079238, 3.0,6.0,10.0, 20.0]
+#!/bin/bash
 sbatch -o ./cluster_outs/IGCSim-%j.out --mail-type=FAIL --mail-user=xjw1001001@qq.com ./ShFiles/PAMLEDNECP_0.0.sh 
 sbatch -o ./cluster_outs/IGCSim-%j.out --mail-type=FAIL --mail-user=xjw1001001@qq.com ./ShFiles/PAMLEDNECP_0.4079238.sh 
 sbatch -o ./cluster_outs/IGCSim-%j.out --mail-type=FAIL --mail-user=xjw1001001@qq.com ./ShFiles/PAMLEDNECP_0.1.sh 
