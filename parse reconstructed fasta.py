0491cb614ebd2feb010fb9dd7c7c807d73695cad
xjw1001001 <1176434052@qq.com>
2018-04-10 12:24:26 +0800

    111

--- parse reconstructed fasta.py
+++ parse reconstructed fasta.py
@@ -4,6 +4,101 @@ Overall accuracy of the 24 ancestral sequences:
 Created on Thu Aug 10 08:23:33 2017
 
 @author: xjw1001001
+"""'''
+#only when PAML in desktop is available,the yeast version only
+import os
+def read_rst(filename,filename1,filename2):
+    f = open(filename,"r")  
+    f1 = open(filename1,"w+")  
+    f2 = open(filename2,"w+")  
+    lines = f.readlines()#读取全部内容
+
+    tree1_flag1 = 0
+    tree2_flag1 = 0
+    tree1_flag2 = 0
+    tree2_flag2 = 0
+    tree1_flag3 = 0
+    flag = 0
+    lines_index = 0
+    for line in lines:
+        lines_index = lines_index + 1
+        if flag == 0:
+            if line == 'List of extant and reconstructed sequences\n':
+                tree1_flag1 = lines_index-1#flag1+4
+                flag = flag + 1
+                continue
+        if flag == 1:
+            if line == 'Overall accuracy of the 23 ancestral sequences:\n':
+                tree1_flag2 = lines_index-1#flag2-3
+                continue
+            if line == 'List of extant and reconstructed sequences\n' :
+                flag = flag + 1
+                continue
+        if flag == 2:
+            if line == 'List of extant and reconstructed sequences\n':
+                tree2_flag1 = lines_index-1#flag1+4
+                flag = flag + 1
+                continue
+        if flag == 3: 
+            if line == 'Overall accuracy of the 23 ancestral sequences:\n' :
+                tree2_flag2 = lines_index-1#flag2-3
+                break 
+    lines_index = 0                    
+    for line in lines:  
+        if len(line)>= 5:
+            if lines_index >= tree1_flag1+4 and lines_index <= tree1_flag2-3:
+                if len(line)>=10:
+                    f1.write(line)
+            if lines_index >= tree2_flag1+4 and lines_index <= tree2_flag2-3:
+                if len(line)>=10:
+                    f2.write(line)
+        lines_index = lines_index + 1
+    f.close()
+    f1.close()
+    f2.close()
+
+
+pair = ['EDN','ECP']
+tau_list = [0.0,0.1,  0.7, 1.0, 0.4079238,3.0,6.0, 10.0, 20.0]
+IGC_geo_list = [3.0, 10.0, 50.0, 100.0, 500.0]
+
+path = '/Users/xjw1001001/Documents/GitHub/IGCsimulation2/PAMLresult/'+'_'.join(pair)+'/'
+
+for tau in tau_list:
+    for IGCgeo in IGC_geo_list:
+        for sim in range(30):
+            primalline=[]
+            fastaline=[]
+            rstpath = path   +'tau' + str(tau) + '/IGCgeo_' + str(IGCgeo) + '/sim_' + str(sim) +'/'
+            read_rst(rstpath + 'rst',rstpath + 'rst_tree1',rstpath + 'rst_tree2')
+            with open(rstpath + 'rst_tree1','r') as f:
+                for line in f.readlines():
+                    primalline.append(line)
+                    sline = '>' + line
+                    sline=sline.replace('node #26','N0'+pair[0])
+                    sline=sline.replace(' ','')
+                    sline=sline.replace('\n','')
+                    for i in range(11):
+                        sline=sline.replace('node#' + str(26+1+i),'N'+str(1+i)+pair[0])
+                        sline=sline.replace('node#' + str(37+1+i),'N'+str(1+i)+pair[1])
+                    sline=sline.replace(pair[0],pair[0] + '\n')
+                    sline=sline.replace(pair[1],pair[1] + '\n')
+                    fastaline.append(sline)    
+            f1 = open('/Users/xjw1001001/Documents/GitHub/IGCsimulation2/test/Ancestral_reconstruction/series/'+'_'.join(pair)+'/'
+                      +'tau' + str(tau) + '/IGCgeo_' + str(IGCgeo) + '/sim_' + str(sim) + '/ancestral_reconstruction_' + '_'.join(pair) + '_PAML.fasta','w+')
+            for line in fastaline:
+                f1.write(line)
+                f1.write('\n')
+            f1.close()
+
+
+''''''
+# -*- coding: utf-8 -*-
+"""List of extant and reconstructed sequences
+Overall accuracy of the 24 ancestral sequences:
+Created on Thu Aug 10 08:23:33 2017
+
+@author: xjw1001001
 """
 #only when PAML in desktop is available,the yeast version only
 import os
@@ -46,12 +141,12 @@ def read_rst(filename,filename1,filename2):
     lines_index = 0                    
     for line in lines:  
         if len(line)>= 5:
-            if lines_index >= tree1_flag1+4 and lines_index <= tree1_flag2-4:
-                f1.write(line)
-                f1.write('\n')
-            if lines_index >= tree2_flag1+4 and lines_index <= tree2_flag2-4:
-                f2.write(line)
-                f2.write('\n')
+            if lines_index >= tree1_flag1+4 and lines_index <= tree1_flag2-3:
+                if len(line)>=10:
+                    f1.write(line)
+            if lines_index >= tree2_flag1+4 and lines_index <= tree2_flag2-3:
+                if len(line)>=10:
+                    f2.write(line)
         lines_index = lines_index + 1
     f.close()
     f1.close()
@@ -75,111 +170,102 @@ for tau in tau_list:
                 for line in f.readlines():
                     primalline.append(line)
                     sline = '>' + line
-                    sline=sline.replace('node #14','Root'+pair[0])
+                    sline=sline.replace('node #14','N0'+pair[0])
                     sline=sline.replace(' ','')
                     sline=sline.replace('\n','')
-                    sline=sline.replace('node#15','N0'+pair[0])
                     for i in range(5):
-                        sline=sline.replace('node#' + str(15+1+i),'N'+str(1+i)+pair[1])
-                        sline=sline.replace('node#' + str(20+1+i),'N'+str(1+i)+pair[0])
+                        sline=sline.replace('node#' + str(14+1+i),'N'+str(1+i)+pair[0])
+                        sline=sline.replace('node#' + str(19+1+i),'N'+str(1+i)+pair[1])
                     sline=sline.replace(pair[0],pair[0] + '\n')
                     sline=sline.replace(pair[1],pair[1] + '\n')
                     fastaline.append(sline)    
-            f1 = open('/Users/xjw1001001/Documents/GitHub/IGCsimulation2/test/Ancestral_reconstruction/series/YDR418W_YEL054C/'
+            f1 = open('/Users/xjw1001001/Documents/GitHub/IGCsimulation2/test/Ancestral_reconstruction/series/'+'_'.join(pair)+'/'
+                      +'tau' + str(tau) + '/IGCgeo_' + str(IGCgeo) + '/sim_' + str(sim) + '/ancestral_reconstruction_' + '_'.join(pair) + '_PAML.fasta','w+')
+            for line in fastaline:
+                f1.write(line)
+                f1.write('\n')
+            f1.close()'''
+import os
+def read_rst(filename,filename1,filename2):
+    f = open(filename,"r")  
+    f1 = open(filename1,"w+")  
+    f2 = open(filename2,"w+")  
+    lines = f.readlines()#读取全部内容
+
+    tree1_flag1 = 0
+    tree2_flag1 = 0
+    tree1_flag2 = 0
+    tree2_flag2 = 0
+    tree1_flag3 = 0
+    flag = 0
+    lines_index = 0
+    for line in lines:
+        lines_index = lines_index + 1
+        if flag == 0:
+            if line == 'List of extant and reconstructed sequences\n':
+                tree1_flag1 = lines_index-1#flag1+4
+                flag = flag + 1
+                continue
+        if flag == 1:
+            if line == 'Overall accuracy of the 19 ancestral sequences:\n':
+                tree1_flag2 = lines_index-1#flag2-3
+                continue
+            if line == 'List of extant and reconstructed sequences\n' :
+                flag = flag + 1
+                continue
+        if flag == 2:
+            if line == 'List of extant and reconstructed sequences\n':
+                tree2_flag1 = lines_index-1#flag1+4
+                flag = flag + 1
+                continue
+        if flag == 3: 
+            if line == 'Overall accuracy of the 19 ancestral sequences:\n' :
+                tree2_flag2 = lines_index-1#flag2-3
+                break 
+    lines_index = 0                    
+    for line in lines:  
+        if len(line)>= 5:
+            if lines_index >= tree1_flag1+4 and lines_index <= tree1_flag2-3:
+                if len(line)>=10:
+                    f1.write(line)
+            if lines_index >= tree2_flag1+4 and lines_index <= tree2_flag2-3:
+                if len(line)>=10:
+                    f2.write(line)
+        lines_index = lines_index + 1
+    f.close()
+    f1.close()
+    f2.close()
+
+
+pair = ['MR','GR']
+tau_list = [0.0,0.5,  1.0, 0.1630137]
+IGC_geo_list = [3.0, 10.0, 50.0, 100.0, 500.0]
+
+path = '/Users/xjw1001001/Documents/GitHub/IGCsimulation2/PAMLresult/'+'_'.join(pair)+'/'
+
+for tau in tau_list:
+    for IGCgeo in IGC_geo_list:
+        for sim in range(30):
+            primalline=[]
+            fastaline=[]
+            rstpath = path   +'tau' + str(tau) + '/IGCgeo_' + str(IGCgeo) + '/sim_' + str(sim) +'/'
+            read_rst(rstpath + 'rst',rstpath + 'rst_tree1',rstpath + 'rst_tree2')
+            with open(rstpath + 'rst_tree1','r') as f:
+                for line in f.readlines():
+                    primalline.append(line)
+                    sline = '>' + line
+                    sline=sline.replace('node #22','N0'+pair[0])
+                    sline=sline.replace(' ','')
+                    sline=sline.replace('\n','')
+                    for i in range(9):
+                        sline=sline.replace('node#' + str(22+1+i),'N'+str(1+i)+pair[0])
+                        sline=sline.replace('node#' + str(31+1+i),'N'+str(1+i)+pair[1])
+                    sline=sline.replace(pair[0],pair[0] + '\n')
+                    sline=sline.replace(pair[1],pair[1] + '\n')
+                    fastaline.append(sline)    
+            f1 = open('/Users/xjw1001001/Documents/GitHub/IGCsimulation2/test/Ancestral_reconstruction/series/'+'_'.join(pair)+'/'
                       +'tau' + str(tau) + '/IGCgeo_' + str(IGCgeo) + '/sim_' + str(sim) + '/ancestral_reconstruction_' + '_'.join(pair) + '_PAML.fasta','w+')
             for line in fastaline:
                 f1.write(line)
                 f1.write('\n')
             f1.close()
-'''
-#ERa_ERb
-pair = ['ERa','ERb']
-primalline=[]
-fastaline=[]
-substitution_dict = {'node#39':'N14ERa','node#38':'N8ERa','node#37':'N7ERa','node#36':'N6ERa','node#41':'N9ERa','node#40':'N5ERa'
-                     ,'node#35':'N4ERa','node#44':'N13ERa','node#46':'N12ERa','node#47':'N11ERa','node#45':'N10ERa'
-                     ,'node#43':'N3ERa','node#42':'N2ERa','node#34':'N1ERa'
-                     ,'node#53':'N14ERb','node#52':'N8ERb','node#51':'N7ERb','node#50':'N6ERb','node#55':'N9ERb','node#54':'N5ERb'
-                     ,'node#49':'N4ERb','node#58':'N13ERb','node#60':'N12ERb','node#61':'N11ERb','node#59':'N10ERb'
-                     ,'node#57':'N3ERb','node#56':'N2ERb','node#48':'N1ERb'}
-with open('/Users/xjw1001001/Desktop/PAML/output/' + '_'.join(pair) +'/out/construct.fasta','r') as f:
-    for line in f.readlines():
-        primalline.append(line)
-        sline = '>' + line
-        sline=sline.replace('node #32','Root'+pair[0])
-        sline=sline.replace(' ','')
-        sline=sline.replace('\n','')
-        sline=sline.replace('node#33','N0'+pair[0])
-        for i in substitution_dict.keys():
-            sline=sline.replace(i,substitution_dict[i])
-        sline=sline.replace(pair[0],pair[0] + '\n')
-        sline=sline.replace(pair[1],pair[1] + '\n')
-        fastaline.append(sline)      
-f1 = open('/Users/xjw1001001/Desktop/PAML/PAMLfasta/PAML_' + '_'.join(pair) +'.fasta','w+')
-for line in fastaline:
-    f1.write(line)
-    f1.write('\n')
-f1.close()
-
-#ARa_ERa
-pair = ['ARa','ERa']
-primalline=[]
-fastaline=[]
-substitution_dict = {'node#36':'N12ERa','node#35':'N11ERa','node#34':'N7ERa','node#33':'N6ERa','node#32':'N5ERa','node#37':'N8ERa'
-                     ,'node#31':'N4ERa','node#41':'N10ERa','node#40':'N9ERa','node#39':'N3ERa','node#38':'N2ERa'
-                     ,'node#30':'N1ERa'
-                     ,'node#48':'N12ARa','node#47':'N11ARa','node#46':'N7ARa','node#45':'N6ARa','node#44':'N5ARa','node#49':'N8ARa'
-                     ,'node#43':'N4ARa','node#53':'N10ARa','node#52':'N9ARa','node#51':'N3ARa','node#50':'N2ARa'
-                     ,'node#42':'N1ARa','node#29':'N0ERa','node#28':'RootERa'}
-with open('/Users/xjw1001001/Desktop/PAML/output/' + '_'.join(pair) +'/out/construct.fasta','r') as f:
-    for line in f.readlines():
-        primalline.append(line)
-        sline = '>' + line
-        sline=sline.replace(' ','')
-        sline=sline.replace('\n','')
-        for i in substitution_dict.keys():
-            sline=sline.replace(i,substitution_dict[i])
-        sline=sline.replace(pair[0],pair[0] + '\n')
-        sline=sline.replace(pair[1],pair[1] + '\n')
-        fastaline.append(sline)      
-f1 = open('/Users/xjw1001001/Desktop/PAML/PAMLfasta/PAML_' + '_'.join(pair) +'.fasta','w+')
-for line in fastaline:
-    f1.write(line)
-    f1.write('\n')
-f1.close()
-
-#ARGRMRPR
-pairlist = [['AR', 'MR'],
-                     ['AR', 'GR'],
-                     ['AR', 'PR'],
-                     ['MR', 'GR'],
-                     ['MR', 'PR'],
-                     ['PR', 'GR']]
-for pair in pairlist:
-    primalline=[]
-    fastaline=[]
-    substitution_dict = {'node#25':'N4'+pair[0],'node#31':'N9'+pair[0],'node#30':'N7'+pair[0]
-                         ,'node#32':'N8'+pair[0],'node#29':'N6'+pair[0],'node#28':'N5'+pair[0]
-                         ,'node#27':'N3'+pair[0],'node#26':'N2'+pair[0],'node#24':'N1'+pair[0]
-                         ,'node#34':'N4'+pair[1],'node#40':'N9'+pair[1],'node#39':'N7'+pair[1]
-                         ,'node#41':'N8'+pair[1],'node#38':'N6'+pair[1],'node#37':'N5'+pair[1]
-                         ,'node#36':'N3'+pair[1],'node#35':'N2'+pair[1],'node#33':'N1'+pair[1]
-                         ,'node#23':'N0'+pair[0],'node#22':'ROOT'+pair[0]
-                         }
-    with open('/Users/xjw1001001/Desktop/PAML/output/' + '_'.join(pair) +'/out/construct.fasta','r') as f:
-        for line in f.readlines():
-            primalline.append(line)
-            sline = '>' + line
-            sline=sline.replace(' ','')
-            sline=sline.replace('\n','')
-            for i in substitution_dict.keys():
-                sline=sline.replace(i,substitution_dict[i])
-            sline=sline.replace(pair[0],pair[0] + '\n')
-            sline=sline.replace(pair[1],pair[1] + '\n')
-            fastaline.append(sline)      
-    f1 = open('/Users/xjw1001001/Desktop/PAML/PAMLfasta/PAML_' + '_'.join(pair) +'.fasta','w+')
-    for line in fastaline:
-        f1.write(line)
-        f1.write('\n')
-    f1.close()
-'''
